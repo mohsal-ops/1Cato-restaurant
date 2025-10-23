506cb9369f8d8a1f65f72bf9e3a6c5d99c3b2c7f
MohSal <bensa0016@gmail.com>
2025-10-23 20:51:15 +0100

    Remove service-account.json, use env variable

--- src/app/api/analytics/route.ts
+++ src/app/api/analytics/route.ts
@@ -1,32 +1,30 @@
 import { google } from "googleapis";
 import { NextRequest, NextResponse } from "next/server";
-import fs from "fs";
-import { json } from "stream/consumers";
 
 const analytics = google.analyticsdata("v1beta");
 
-export  async function GET(req : NextRequest, res :NextResponse ) {
-    try {
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_KEY!);
 
-        const auth = new google.auth.GoogleAuth({
-            keyFile: "D:/next-js/projects/restaurant/src/app/admin/service-account.json", // Replace with your service account key
-            scopes: ["https://www.googleapis.com/auth/analytics.readonly"],
-        });
+    const auth = new google.auth.GoogleAuth({
+      credentials: serviceAccount, // <- use credentials instead of keyFile
+      scopes: ["https://www.googleapis.com/auth/analytics.readonly"],
+    });
 
-        const analyticsClient = await analytics.properties.runReport({
-            property: "properties/479278566", // Replace with your GA property ID
-            auth,
-            requestBody: {
-                dimensions: [{ name: "sessionDefaultChannelGrouping" }],
-                metrics: [{ name: "sessions" },{name:'activeUsers'}],
-                dateRanges: [{ startDate: "30daysAgo", endDate: "today" }],
-            },
-        });
-        return new Response(JSON.stringify(analyticsClient.data))
+    const analyticsClient = await analytics.properties.runReport({
+      property: "properties/479278566", // your GA property ID
+      auth,
+      requestBody: {
+        dimensions: [{ name: "sessionDefaultChannelGrouping" }],
+        metrics: [{ name: "sessions" }, { name: "activeUsers" }],
+        dateRanges: [{ startDate: "30daysAgo", endDate: "today" }],
+      },
+    });
 
-    } catch (error) {
-        console.log('error of fetching analytics shit',error)
-        console.error(error);
-        return new Response(JSON.stringify(error))
-    }
+    return new Response(JSON.stringify(analyticsClient.data));
+  } catch (error) {
+    console.error("Error fetching analytics:", error);
+    return new Response(JSON.stringify(error));
+  }
 }
